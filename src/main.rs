512bb60182bbdc3b879b57145f03798f53a9a63e
caveman-george <george@caveman.im>
2025-04-17 23:10:23 +0100

    first experience with reading the cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use std::env;
+
+fn show_menu() {
+    println!("RobCo Assistant");
+    println!("1. Create a task");
+    println!("2. View tasks");
+    println!("3. Edit a task");
+    println!("4. Delete a task");
+    println!("5. Exit");
+    println!("Please select an option:");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("Your command line arguments are: {:?}", args);
+        return;
+    }
+
+    show_menu();
+}
